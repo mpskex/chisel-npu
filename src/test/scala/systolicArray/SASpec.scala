c1fb06cb9dfffc66342036d42b1f9e19dd21bce2
Fangrui.Liu <fangrui.liu@ubc.ca>
2024-02-15 18:23:06 +0800

    updated unittest for systolic array. shorten the latency with zero bubble.

--- src/test/scala/systolicArray/SASpec.scala
+++ src/test/scala/systolicArray/SASpec.scala
@@ -65,11 +65,14 @@ class SASpec extends AnyFlatSpec with ChiselScalatestTester {
             val _mat_a = new Array[Int](_n * _n)
             val _mat_b = new Array[Int](_n * _n)
             val _expected = new Array[Int](_n * _n)
+            var _res = new Array[Int](_n * _n)
+            
             // random initialize the
             for (i <- 0 until _n * _n) {
                 _mat_a(i) = rand.between(0, 255)
                 _mat_b(i) = rand.between(0, 255)
             }
+
             // expected matrix multiplication result
             for (_i <- 0 until _n) {
                 for (_j <- 0 until _n) {
@@ -78,6 +81,7 @@ class SASpec extends AnyFlatSpec with ChiselScalatestTester {
                     }
                 }
             }
+
             // print the expected results
             println("===== MAT A =====")
             this.printMatrix(_mat_a, _n)
@@ -87,7 +91,7 @@ class SASpec extends AnyFlatSpec with ChiselScalatestTester {
             this.printMatrix(_expected, _n)
 
             // systolic arrays has latency of 3 * _n - 2
-            for (i_tick <- 0 to 3 * _n - 2) {
+            for (i_tick <- 0 until 3 * _n - 2) {
                 val _a_in_t = new Array[Int](_n)
                 val _b_in_t = new Array[Int](_n)
                 // initialize tensor dispatch 
@@ -100,6 +104,7 @@ class SASpec extends AnyFlatSpec with ChiselScalatestTester {
                     }
                 }
 
+                // print input layout for each tick
                 var _a_in_str = ""
                 var _b_in_str = ""
                 for (__i <- 0 until _n) {
@@ -108,34 +113,40 @@ class SASpec extends AnyFlatSpec with ChiselScalatestTester {
                 }
                 println("Vector A tick @ " + i_tick + ": [" + _a_in_str + "]")
                 println("Vector B tick @ " + i_tick + ": [" + _b_in_str + "]")
-                // this.printMatrixChisel(dut.io.out, _n)
 
+                // poke the input vector
                 for (_i <- 0 until _n){
                     dut.io.vec_a(_i).poke(_a_in_t(_i))
                     dut.io.vec_b(_i).poke(_b_in_t(_i))
                 }
 
-                if (i_tick < 2 * _n - 1 && i_tick > 0) 
+                // Only the first _n ticks need accumlate signal
+                // The rest of the control signal will hand over
+                // to a dedicated systolic-ish control bus
+                if (i_tick < _n && i_tick >= 0)
                     dut.io.ctrl.poke(0x1)
                 else
-                    dut.io.ctrl.poke(0x1)
+                    dut.io.ctrl.poke(0x0)
+
+                // ideally, the array will give _n (diagnal) results per tick
                 dut.clock.step()
-            }
-            println("+++++ MAT C from HW +++++")
-            this.printMatrixChisel(dut.io.out, _n)
-            for (i <- 0 until _n) {
-                for (j <- 0 until _n) {
-                    dut.io.out(i * _n + j).expect(_expected(i * _n + j))
+
+                // systolic array will start to spit out after _n - 1 ticks
+                if (i_tick >= _n - 1) {
+                    for (_i <- 0 until _n) {
+                        for (_j <- 0 until _n) {
+                            // And the array will give result in 2 * _n - 1 ticks
+                            if (_i + _j == i_tick - _n + 1) {
+                                dut.io.out(_i * _n + _j).expect(_expected(_i * _n + _j))
+                                _res(_i * _n + _j) = dut.io.out(_i * _n + _j).peekInt().toInt
+                                println("Tick @ " + i_tick + " producing at location (" + _i + ", " + _j + ")")
+                            }
+                        }
+                    }
                 }
             }
+            println("+++++ MAT C from HW ++++")
+            this.printMatrix(_res, _n)
         }
-    }
-
-    // TODO: clarify anti-bubble scheduling
-    "SA" should "avoid schedule dispatch bubbles" in {
-        test(new SystolicArray(4, 8)) { dut =>
-            val _n = 4
-        }
-    }
-        
+    }   
 }
