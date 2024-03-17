2fd67a906c82ddd69a4a61603b409ca3263c63b7
Fangrui.Liu <fangrui.liu@ubc.ca>
2024-03-17 23:49:20 +0800

    fix expect

--- src/test/scala/ncore/mmu/MMUSpec.scala
+++ src/test/scala/ncore/mmu/MMUSpec.scala
@@ -23,13 +23,26 @@ class MMUSpec extends AnyFlatSpec with ChiselScalatestTester {
                               List(true, false, true, true), 
                               List(false, true, false, true), 
                               List(false, false, true, false), 
-                              List(false, false, false, true), 
+                              List(false, false, false, true),
                               )
-            for (i <- 0 until 16) {
-                for (j <- 0 until _n){
+            val _expected = List(List(0, 4, 8, 12),
+                                List(1, 4, 8, 12),
+                                List(2, 5, 8, 12),
+                                List(3, 6, 9, 12),
+                                List(0, 7, 10, 13),
+                                List(1, 4, 11, 14),
+                                List(0, 5, 8, 15),
+                                List(0, 4, 9, 12),
+                                List(0, 4, 8, 13),
+            )
+            for (i <- 0 until _array.length) {
+                for (j <- 0 until _n) {
                     dut.io.inc(j).poke(_array(i%_array.length)(j))
                 }
                 dut.clock.step()
+                for (j <- 0 until _n) {
+                    dut.io.out(j).expect(_expected(i)(j))
+                }
                 print_helper.printVectorChisel(dut.io.out, _n)
             }
         }
