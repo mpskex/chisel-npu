9b12a92acc5894e958166b7fd3a206b3aa3e5dbb
Fangrui.Liu <fangrui.liu@ubc.ca>
2024-03-26 23:49:38 +0800

    multi channel input

--- src/main/scala/ncore/tcm/tightCpldMem.scala
+++ src/main/scala/ncore/tcm/tightCpldMem.scala
@@ -24,13 +24,14 @@ class TCMCell(val nbits: Int = 8) extends Module {
 
 class TCMBlock(val n: Int = 8, 
                val size: Int = 4096,
+               val rd_ch_num: Int = 2,
                val nbits: Int = 8
 ) extends Module {
     val io = IO(
         new Bundle {
             val d_in    = Input(Vec(n * n, UInt(nbits.W)))
-            val d_out   = Output(Vec(n * n, UInt(nbits.W)))
-            val r_addr  = Input(Vec(n * n, UInt(log2Ceil(size).W)))
+            val d_out   = Output(Vec(rd_ch_num, Vec(n * n, UInt(nbits.W))))
+            val r_addr  = Input(Vec(rd_ch_num, Vec(n * n, UInt(log2Ceil(size).W))))
             val w_addr  = Input(Vec(n * n, UInt(log2Ceil(size).W)))
             val en_wr   = Input(Bool())
         }
@@ -47,7 +48,9 @@ class TCMBlock(val n: Int = 8,
     //TODO: add read & write conflict check
 
     for (i <- 0 until n * n) {
-        io.d_out(i) := cells_io(io.r_addr(i)).d_out
+        for (k <- 0 until rd_ch_num) {
+            io.d_out(k)(i) := cells_io(io.r_addr(k)(i)).d_out
+        }
         when (io.en_wr) {
             cells_io(io.w_addr(i)).en_wr := io.en_wr
             cells_io(io.w_addr(i)).d_in := io.d_in(i)
@@ -60,22 +63,25 @@ class DetachableTCM(
     val n: Int = 8,
     val nblocks: Int = 4,
     val size: Int = 4096,
+    val rd_ch_num: Int = 2,
 ) extends Module {
     val io = IO(new Bundle {
         val d_in        = Input(Vec(n * n, Vec(nblocks, UInt(8.W))))
-        val d_out       = Output(Vec(n * n, Vec(nblocks, UInt(8.W))))
-        val r_addr      = Input(Vec(n * n, UInt(log2Ceil(size).W)))
+        val d_out       = Output(Vec(rd_ch_num, Vec(n * n, Vec(nblocks, UInt(8.W)))))
+        val r_addr      = Input(Vec(rd_ch_num, Vec(n * n, UInt(log2Ceil(size).W))))
         val w_addr      = Input(Vec(n * n, UInt(log2Ceil(size).W)))
         val en_wr       = Input(Bool())
     })
 
     val tcm_blocks_io  = VecInit(Seq.fill(nblocks) {
-        Module(new TCMBlock(n, size, 8)).io})
+        Module(new TCMBlock(n, size, rd_ch_num, 8)).io})
     
     for (i <- 0 until nblocks) {
         tcm_blocks_io(i).en_wr := io.en_wr
         for (j <- 0 until n) {
-            tcm_blocks_io(i).r_addr(j) := io.r_addr(j)
+            for (k <- 0 until rd_ch_num) {
+                tcm_blocks_io(i).r_addr(k)(j) := io.r_addr(k)(j)
+            }
             tcm_blocks_io(i).w_addr(j) := io.w_addr(j)
             tcm_blocks_io(i).d_in(j) := io.d_in(j)(i)
             io.d_out(j)(i) := tcm_blocks_io(i).d_out(j)
